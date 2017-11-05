13c6ebd8a920b58c31c583b8763e40ba93a80710
Salman <salman.hashmi2@mail.mcgill.ca>
2017-11-05 05:06:20 -0500

    feat: Add method to compute HSV color histograms in features Compute and concatenate HSV color histograms into a single feature vector.

--- sensor_stick/scripts/features.py
+++ sensor_stick/scripts/features.py
@@ -35,3 +35,42 @@ def rgb_to_hsv(rgb_list):
     hsv_normalized = matplotlib.colors.rgb_to_hsv([[rgb_normalized]])[0][0]
     return hsv_normalized
 
+
+def compute_color_histograms(cloud, using_hsv=False):
+    """Compute and concatenate HSV histograms into a single feature vector."""
+    # Compute histograms for the clusters
+    point_colors_list = []
+
+    # Step through each point in the point cloud
+    for point in pc2.read_points(cloud, skip_nans=True):
+        rgb_list = float_to_rgb(point[3])
+        if using_hsv:
+            point_colors_list.append(rgb_to_hsv(rgb_list) * 255)
+        else:
+            point_colors_list.append(rgb_list)
+
+    # Populate lists with color values
+    channel_1_vals = []
+    channel_2_vals = []
+    channel_3_vals = []
+
+    for color in point_colors_list:
+        channel_1_vals.append(color[0])
+        channel_2_vals.append(color[1])
+        channel_3_vals.append(color[2])
+
+    # Compute the histogram of the HSV channels separately
+    nbins = 32
+    bins_range = (-1, 1)
+    h_hist = np.histogram(channel_1_vals, bins=nbins)
+    s_hist = np.histogram(channel_2_vals, bins=nbins)
+    v_hist = np.histogram(channel_3_vals, bins=nbins)
+
+    # Concatenate and normalize the histograms into a single feature vector
+    hist_features = np.concatenate((h_hist[0], s_hist[0], v_hist[0])).astype(np.float64)
+    # Normalize the result
+    normed_features = hist_features / np.sum(hist_features)
+
+    return normed_features
+
+
